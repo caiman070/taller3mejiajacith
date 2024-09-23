35911d1fc084d1244b203b1c314a8cfd8cfd8815
Kevin Escorcia <Kescorci5@gmail.com>
2024-09-23 09:52:32 -0500

    m1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,12 @@ function App() {
     <>
       <div> 
        <img src={nagime} alt="nagime logo" />
+       <p>
+       Nagumo Hajime, es el protagonista principal de la novela web/ligera, manga, y anime Arifureta Shokugyou de Sekai Saikyou. 
+       Hajime solía ser un estudiante de preparatoria, el típico otaku promedio, 
+       que recibía constantemente abuso por parte de la mayoría de sus compañeros. Un día, toda su clase fue transportada a otro mundo;
+       Tortus, un mundo de fantasía en donde existían humanos, semi-humanos y demonios.
+       </p>
        <p >
         taller de jacith mejia 
        </p>
