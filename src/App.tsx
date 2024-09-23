da0a716de19b9b794a06b55406a9b03715e6a7bd
Kevin Escorcia <Kescorci5@gmail.com>
2024-09-23 09:16:41 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import nagime from './assets/img/nagime.png'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div> 
+       <img src={nagime} alt="nagime logo" />
+       <p >
+        taller de jacith mejia 
+       </p>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
